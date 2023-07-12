e32b0126260a58ab8280b168a1dc9c1179ad5028
katemcg <katemcg@umich.edu>
2023-07-12 13:01:06 -0400

    Return to main menu button on horoscopes page

--- pages/horoscope.js
+++ pages/horoscope.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { getSession } from 'next-auth/react';
 import Head from 'next/head';
 import {makePrompt} from '/lib/makePrompt.js'
+import Link from 'next/link';
 
 let apiCall = false;
 
@@ -141,6 +142,15 @@ const HoroscopePage = () => {
                 </div>
             )}
             </main>
+            <footer className="fixed bottom-0 left-0 right-0 bg-gray-200 py-2">
+                <div className="flex justify-center">
+                    <Link legacyBehavior href="/">
+                    <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Return to Main Menu
+                    </a>
+                    </Link>
+                </div>
+            </footer>
      </div>
       );
       
