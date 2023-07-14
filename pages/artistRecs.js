95ea468965a4e48303e29420ebd2bb1e19e436c4
katemcg <katemcg@umich.edu>
2023-07-14 13:59:17 -0400

    Working version of get artist recs

--- pages/artistRecs.js
+++ pages/artistRecs.js
@@ -1,17 +1,97 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import { getSession } from 'next-auth/react';
 import Head from 'next/head';
 import Link from 'next/link';
-
-
+import {makePrompt} from '/lib/makePrompt.js'
+import { makePromptArtistRecs } from '@/lib/makePrompt';
 /* TODO:
 -fetch users top artists
 -ask open ai for 5 artist recs based on this
 -search for spotify artist ID with spotify search api
 -display artist and image of artist*/
 const artistRecs = () => {
-   
+  const [artists, setArtists] = useState([]);
+  const [recs, setRecs] = useState('');
+  let generatingFuncCalled = useRef(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const session = await getSession();
+      const accessToken = session?.accessToken;
+
+      if (!accessToken) {
+        // Handle case when access token is not available
+        return;
+      }
+
+      try {
+        const artistsResponse = await axios.get(
+          'https://api.spotify.com/v1/me/top/artists',
+          {
+            headers: {
+              Authorization: `Bearer ${session.accessToken}`,
+            },
+          }
+        );
+        const artistsData = artistsResponse.data.items;
+
+
+        setArtists(artistsData);
+      } catch (error) {
+        console.error('Error retrieving favorite artists and genres:', error);
+        // Handle error appropriately
+      }
+    };
+
+    if (artists.length === 0) {
+      fetchData();
+    }
+  }, []); 
+
+
+
+  useEffect(() => {
+        
+    const generateRecs = async () => {
+
+       
+
+      console.log('generatingRecs...');
+      try {
+        const prompt = makePromptArtistRecs(
+          artists.map((artist) => artist.name)
+        );
+        const response = await fetch('/api/openai', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ prompt }),
+        });
+        const data = await response.json();
+        setRecs(data.result);
+        
+
+
+      } catch (error) {
+        console.error('Error generating recommendations:', error);
+        // Handle error appropriately
+      }
+    };
+
+
+    if (artists.length > 0  &&recs === '') {
+        if (!generatingFuncCalled.current) { // access the current value of the ref using .current
+            generatingFuncCalled.current = true;
+            generateRecs();
+        }
+    }
+  }, [artists]);
+
+
+
+
     return (
       <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
         <Head>
@@ -19,7 +99,21 @@ const artistRecs = () => {
         </Head>
     
         <main className="flex flex-col items-center justify-center h-screen bg-gray-100">
-    This page is still in development.
+          <div className="text-4xl font-bold text-black">
+            {recs ? (
+            <span></span>
+            ) : (
+            <span>Getting artist recommendations...</span>
+            )}
+        </div>
+        {recs && (
+            <div className="mt-4">
+            <p className="text-2xl text-black font-bold">Our Recommendations:</p>
+            <div className="bg-purple-500 rounded-lg p-4 mt-2 inline-block">
+                <h1 className="text-3xl font-semibold text-white">{recs}</h1>
+            </div>
+            </div>
+                  )}
         </main>
 
 
